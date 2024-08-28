5bc56808903f1fded61405191fcddc0f6b5deb4e
xyyzh <zxinyi1215@gmail.com>
2024-08-27 22:33:15 -0400

    initial commit

---
+++ src/main/java/com/example/IngreWiz/IngreWizApplication.java
@@ -0,0 +1,13 @@
+package com.example.IngreWiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngreWizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngreWizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/IngreWiz/IngreWizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IngreWiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngreWizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
