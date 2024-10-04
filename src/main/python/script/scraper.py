67d5fbc83181e08e036cdd283252b57be177872b
xyyzh <zxinyi1215@gmail.com>
2024-10-03 20:08:04 -0400

    ipynb initial commit

---
+++ src/main/python/script/scraper.py
@@ -0,0 +1,18 @@
+# from recipe_scrapers import scrape_me
+
+# # URL of the recipe to scrape
+# url = 'https://www.allrecipes.com/recipe/12345'
+
+# # Scrape the recipe with the Allrecipes schema
+# scraper = scrape_me(url, wild_mode=False)
+
+# # Print the recipe title
+# print(scraper.title())
+
+# # Print the recipe ingredients
+# print(scraper.ingredients())
+
+# # Print the recipe instructions
+# print(scraper.instructions())
+
+print("Hi from Python")
